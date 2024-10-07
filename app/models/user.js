1676c2e83007711f05aec3d5baf3692fb6396baa
Eamonn Smyth <59707387+Mr-Smyth@users.noreply.github.com>
2024-10-07 18:53:47 +0100

    Update user.js

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class UserModel extends Model {
-  @attr('string') first_name; // Ensure the attribute name matches the payload
-  @attr('string') last_name; // Ensure the attribute name matches the payload
-  @attr('string') description; // Ensure the attribute name matches the payload
+  @attr('string') first_name;
+  @attr('string') last_name;
+  @attr('string') description;
 }
